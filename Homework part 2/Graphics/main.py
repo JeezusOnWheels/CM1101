4e6b1f1ff4a64f4b14bc173cbcb991a7e1ef895c
JeezusOnWheels <38922533+JeezusOnWheels@users.noreply.github.com>
2018-10-13 20:21:25 +0400

    Delete main.py

--- Homework part 2/Graphics/main.py
+++
@@ -1,7 +0,0 @@
-import graphics
-win = graphics.GraphWin("My Circle", 100, 100)
-c = graphics.Circle(graphics.Point(50,50), 10)
-c.draw(win)
-win.getMouse() # Pause to view result
-win.close()    # Close window when done
-print ("Hello world")
